b5bb3b6da9b50c6e6e31b8068410ec2f995590c6
HegyiBazsi <h.bazsi940802@gmail.com>
2018-05-13 09:35:30 +0200

    Revert "harmadik iteracio v2"
    This reverts commit 101a3ee2c8315f669e47e8bc74aed0e96067c3c2.

--- Harmadik iteráció/v1/Client.java
+++ Harmadik iteráció/v1/Client.java
@@ -1,7 +1,4 @@
 import java.io.*;
-import java.net.*;
-import java.util.*;
-import java.lang.*;
 
 class Client
 {
@@ -14,36 +11,52 @@ class Client
 
 	/*---------- V A L T O Z O K ----------*/
 	public String inputfile="users.txt";
-	public List<User> users;
-	public int choice;
-	public boolean finishclient;
-	public int jobazon;
+	public List<Users> users;
 
 	public Client()
 	{
-			users = new ArrayList<User>();
+			users = new ArrayList<Users>();
 	}
 
 	private void start()
 	{
 		try
 		{
-		  finishclient = false;
+			boolean finishclient = false;
 			while(! finishclient)
 			{
-				System.out.println();
-				System.out.print("Kerem valassza ki mit szeretne csinalni(1. belepes, 2. Regisztacio, 3. Kilepes a programbol): ");
-				int option = getUserInput();
-				System.out.println();
-				switch(option)
+				System.out.println("Kerem valassza ki a belepo tipusat: (1. Diszpecser, 2. Raktaros 3. Konyvelo 4. Kilepes)");
+				int choice = getUserInput();
+				switch(choice)
 				{
 					case 1:
-						login();
+						Diszpecser diszpecser = new Diszpecser();
+						finished = false;
+						while(! finished)
+						{
+							diszpecser.showMenu();
+							finished = diszpecser.executeServer();
+						}
 					break;
 					case 2:
-						register();
+						Raktaros raktaros = new Raktaros();
+						finished = false;
+						while(! finished)
+						{
+							raktaros.showMenu();
+							finished=raktaros.executeServer();
+						}
 					break;
 					case 3:
+						Konyvelo konyvelo = new Konyvelo();
+						finished = false;
+						while(! finished)
+						{
+							konyvelo.showMenu();
+							finished=konyvelo.executeServer();
+						}
+					break;
+					case 4:
 						finishclient= true;
 					break;
 				}//end of switch
@@ -55,36 +68,62 @@ class Client
 		}
 
 
-	}//end of start
+	}
 
-	/*---------- L O G I N ----------*/
-	public void login()
+	/*---------- I N T   I N P U T ----------*/
+	private int getUserInput()throws IOException
 	{
-		try
-		{
-			try
-			{
-					read();
-			}
-			catch(StringIndexOutOfBoundsException ex)
-			{
-				System.out.println("Users File is empty!");
-			}
-		}
-		catch(IOException ex)
-		{
-			System.out.println("Could not read file!");
-		}
-		try
-		{
-			System.out.println();
-			System.out.print("Kerem adja meg a felhasznalo nevet: ");
-			String username = readUserInput();
-			System.out.print("Kerem adja meg a jelszavat: ");
-			String password = readUserInput();
-			System.out.println();
-			Iterator<User> i = users.iterator();
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		String line = input.readLine();
+		return Integer.valueOf(line);
+	}
 
+	/*---------- S T R I N G   I N P U T ----------*/
+  	private String readUserInput()throws IOException
+  	{
+  		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+  		String line = input.readLine();
+  		return line;
+  	}
+
+	/*---------- R E A D ----------*/
+		private void read() throws IOException
+		{
+			BufferedReader be = new BufferedReader(new FileReader(inputfile));
+			String in = new String("");
+			users.clear();
+			while(!((in=be.readLine())==null))
+			{
+				String[] line= in.split("\\,");
+				for(int i=0;i<line.length;i++)
+				{
+					String[] adat = line[i].split("\\|");
+					adat[0]=adat[0].substring(1,adat[0].length());
+					if(i==line.length-1)
+					{
+							adat[2]=adat[2].substring(0,adat[2].length()-1);
+					}
+					try
+					{
+						users.add(new User(adat[0], adat[1], adat[2]));
+					}
+					catch(NumberFormatException ex)
+					{
+						System.out.println("Sikertelen beolvasas!");
+					}
+				}
+			}
+		}// end of readLine
+
+		public void login()
+		{
+			read();
+			System.out.println("Kerem adja meg a felhasznalo nevet:");
+			String username = readUserInput();
+			System.out.println("Kerem adja meg a jelszavat:");
+			String password = readUserInput();
+			Iterator<User> i = users.iterator();
+			int choice;
 			while(i.hasNext())
 			{
 				User element = i.next();
@@ -124,132 +163,58 @@ class Client
 					}
 				break;
 				case 4:
-					boolean finishclient= true;
+					finishclient= true;
 				break;
 			}//end of switch
-		}
-		catch(IOException ex)
-		{
-			System.out.println("Not valid input!");
-		}
-	}//end of login
 
-/*---------- R E G I S T E R ----------*/
-	public void register()
-	{
-		try
-		{
-			try
-			{
-					read();
-			}
-			catch(StringIndexOutOfBoundsException ex)
-			{
-				System.out.println("Users File is empty!");
-			}
-		}
-		catch(IOException ex)
-		{
-			System.out.println("Could not read file!");
-		}
+		}//end of login
 
-		try
+		public void register()
 		{
-			System.out.println();
-			System.out.print("Kerem adja meg a felhasznalo nevet: ");
+			read();
+			System.out.println("Kerem adja meg a felhasznalo nevet:");
 			String username = readUserInput();
-			System.out.print("Kerem adja meg a jelszavat: ");
+			System.out.println("Kerem adja meg a jelszavat:");
 			String password = readUserInput();
-			System.out.print("Kerem adja meg a munkajat ugy ahogy fel van sorolva(Diszpecser/Raktaros/Konyvelo): ");
+			System.out.println("Kerem adja meg a munkajat ugy ahogy fel van sorolva(Diszpecser/Raktaros/Konyvelo):");
 			String title = readUserInput();
-			System.out.println();
-			if(title.equals("Diszpecser"))
+
+			if(title.equals(Diszpecser))
 			{
-				jobazon=1;
+				int title=1;
 			}
-			else if(title.equals("Raktaros"))
+			else if(title.equals(Raktaros))
 			{
-				jobazon=2;
+				int title=2;
 			}
-			else if(title.equals("Konyvelo"))
+			else if(title.equals(Diszpecser))
 			{
-				jobazon=3;
+				int title=3;
 			}
 			else
 			{
 				System.out.println("Rossz adatot adott meg a munkajara.");
 				System.out.println("Kerem adja meg a munkajat ujra ugy ahogy fel van sorolva(Diszpecser/Raktaros/Konyvelo):");
-				title = readUserInput();
-				if(title.equals("Diszpecser"))
+				String title = readUserInput();
+				if(title.equals(Diszpecser))
 				{
-					jobazon=1;
+					int title=1;
 				}
-				else if(title.equals("Raktaros"))
+				else if(title.equals(Raktaros))
 				{
-					jobazon=2;
+					int title=2;
 				}
-				else if(title.equals("Konyvelo"))
+				else if(title.equals(Diszpecser))
 				{
-					jobazon=3;
+					int title=3;
 				}
 			}// end of kiertekeles
 
-			User temp = new User(username,password,jobazon);
+			User temp = new User(username,password,title);
 			users.add(temp);
 			PrintWriter datafile = new PrintWriter(inputfile);
 			datafile.println(users);
 			datafile.flush();
 			datafile.close();
 		}
-		catch(IOException ex)
-		{
-			System.out.println("Not valid input!");
-		}
-	}//end of register
-
-	/*---------- I N T   I N P U T ----------*/
-	private int getUserInput()throws IOException
-	{
-		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-		String line = input.readLine();
-		return Integer.valueOf(line);
-	}
-
-	/*---------- S T R I N G   I N P U T ----------*/
-  	private String readUserInput()throws IOException
-  	{
-  		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-  		String line = input.readLine();
-  		return line;
-  	}
-
-	/*---------- R E A D ----------*/
-		private void read() throws IOException
-		{
-			BufferedReader be = new BufferedReader(new FileReader(inputfile));
-			String in = new String("");
-			users.clear();
-			while(!((in=be.readLine())==null))
-			{
-				String[] line= in.split("\\,");
-				for(int i=0;i<line.length;i++)
-				{
-					String[] adat = line[i].split("\\|");
-					adat[0]=adat[0].substring(1,adat[0].length());
-					if(i==line.length-1)
-					{
-							adat[2]=adat[2].substring(0,adat[2].length()-1);
-					}
-					try
-					{
-						users.add(new User(adat[0], adat[1], Integer.parseInt(adat[2])));
-					}
-					catch(NumberFormatException ex)
-					{
-						System.out.println("Sikertelen beolvasas!");
-					}
-				}
-			}
-		}// end of read
-
-}//end of client class
+}
